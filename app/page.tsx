8d88bbef86e922b1cc714ab1debcb20cfb2510b5
Kriti <kritibhatt09@gmail.com>
2025-07-07 11:59:38 +0530

    Implement Authentication, Billing, Database Setup. Create Comp[anion Server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,55 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>
-        Welcome to my SaaS App
+    <main>
+      <h1>
+        Popular Companions
       </h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura Explorer"
+          topic="Neural Networks of the Brain"
+          subject = "Science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject = "Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language"
+          subject = "English Litrature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames = "w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
